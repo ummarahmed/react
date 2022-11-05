ad89ba45613f6f0d79814b0c0e0b839d92cace36
Ahmed Ummar <ummarahmed001@gmail.com>
2022-11-06 00:52:19 +0200

    4. understanding of JSX

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+// let name = "Ummar";
+let name = prompt("Enter your name: ")
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <p>
+        <h1>Hello {name}, Welcome to Website :)</h1>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, nulla aut, sed ipsam deleniti magni iusto non amet pariatur fuga, delectus minima optio. Vel quam pariatur, alias dolores beatae expedita.
+      </p>
     </div>
+    </>
   );
 }
 
