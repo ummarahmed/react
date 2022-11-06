7aa0ac1c820d32f6b02cc3c7c3670531ec7259c3
Ahmed Ummar <ummarahmed001@gmail.com>
2022-11-06 18:30:59 +0200

    9. Copy, Past, Clear
    Copy data from text area and paste data in text are and clear the text area functionalities are added.

--- textutils/src/componants/TextForm.js
+++ textutils/src/componants/TextForm.js
@@ -22,21 +22,35 @@ export default function TextForm(props) {
         setText(event.target.value)
         
     }
+    const copyTextToClipboard = ()=>{
+        let newText = text;
+        // newText.select();
+        // newText.setSelectionRange(0,99999); // for mobiles
+
+        navigator.clipboard.writeText(newText);
+    }
+    const pasteText = ()=>{
+        let clipText =  navigator.clipboard.readText()
+        .then((pText)=>{
+            //  return text;
+             console.log("Clip Text: "+pText);
+             let newText = text+pText;
+             setText(newText);
+             
+        })
+        .catch((error)=>{
+            //  return ;
+             console.log("error - no text");
+        });
+        // console.log("Clip Text: "+clipText.text);
+    }
+    const clear = ()=>{
+        setText("");
+    }
+
     const [text, setText] = useState('Enter text here');
     // text = "new text Value"; // wrong way
     // setText("New Text updated"); // correct way
-
-    /*
-    // get selected text
-    function getSel(boxId)
-    {
-        var txtarea = document.getElementById(boxId);
-        var start = txtarea.selectionStart;
-        var finish = txtarea.selectionEnd;
-        var sel = txtarea.value.substring(start, finish);
-        return sel
-    }
-    */
 
   return (
     <>
@@ -45,8 +59,11 @@ export default function TextForm(props) {
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUPClick}>UPPERCASE</button>
-        <button className="btn btn-primary mx-3" onClick={handlelowerClick}>lowercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUPClick}>UPPERCASE</button>
+        <button className="btn btn-primary mx-2" onClick={handlelowerClick}>lowercase</button>
+        <button className='btn btn-primary mx-2' onClick={copyTextToClipboard}>Copy to clipboard</button>
+        <button className='btn btn-primary mx-2' onClick={pasteText}>Paste text</button>
+        <button className="btn btn-primary max-2" onClick={clear}>Clear</button>
     </div>
     <div className="container my-3">
         <h2>Your text summary</h2>
