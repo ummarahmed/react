ea60a380f0d5ed1bf7c44f471ccff05847d6daab
Ahmed Ummar <ummarahmed001@gmail.com>
2022-11-06 11:09:34 +0200

    5. Setup and adding Bootstrap components to ReactApp

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-// let name = "Ummar";
-let name = prompt("Enter your name: ")
+
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <div className="container">
-      <p>
-        <h1>Hello {name}, Welcome to Website :)</h1>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, nulla aut, sed ipsam deleniti magni iusto non amet pariatur fuga, delectus minima optio. Vel quam pariatur, alias dolores beatae expedita.
-      </p>
+    <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">TextUtils</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+        
+        
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
+  </div>
+</nav>
     </>
   );
 }
