507aba617fab6c9ac52cf67bdbbc969691d8f5f7
Ahmed Ummar <ummarahmed001@gmail.com>
2022-11-06 13:16:32 +0200

    6. Props and PropTypes
    Understanding of Props and PropTypes

---
+++ textutils/module1.mjs
@@ -0,0 +1,3 @@
+import ui, {a, c, d}  from './module2.mjs'; // use {} when somthing is not exporting by default
+console.log(ui);
+console.log(a+ "\n"+c+"\n"+d)
---
+++ textutils/module2.mjs
@@ -0,0 +1,7 @@
+const a = "Ali";
+const b = "Bilal";
+const c = "Chand";
+const d = "Jekab";
+
+export default b;
+export {a, c, d};
